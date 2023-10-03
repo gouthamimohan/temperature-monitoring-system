50e779944ed40ab9fcd11cf51598b89e692626bb
gouthamim <gouthami.m@ihsmarkit.com>
2023-10-03 20:06:02 +0530

    change1

--- src/app/temperature-alarm.component.ts
+++ src/app/temperature-alarm.component.ts
@@ -1,24 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+
 import { TemperatureEvent } from './temperature.event';
+
 import { TemperatureSensorService } from './temperature-sensor.service';
 
+ 
+
 @Component({
+
   selector: 'app-temperature-alarm',
-  template: '<div>{{ warningMessage }}</div>',
+
+  template: `
+
+    <div [style.background]="'red'">
+
+      Temperature: {{ temperature }}°C<br>{{ warningMessage }}
+
+    </div>
+
+  `,
+
 })
+
 export class TemperatureAlarmComponent implements OnInit {
+
+  temperature = 0;
+
   warningMessage = '';
+
+ 
 
   constructor(private temperatureSensorService: TemperatureSensorService) {}
 
+ 
+
   ngOnInit() {
+
     this.temperatureSensorService
+
       .getTemperatureEvents()
+
       .subscribe((temperatureEvent: TemperatureEvent) => {
+
+        this.temperature = temperatureEvent.temperature;
+
         this.warningMessage =
+
           'Warning! High temperature detected: ' +
-          temperatureEvent.temperature +
+
+          this.temperature +
+
           '°C';
+
       });
+
   }
+
 }
